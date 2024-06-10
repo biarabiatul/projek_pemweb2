8f084b922eae2318a743ea23a0e8eacc88ef8fd0
MNFadhillah <mnurfadhillah31@gmail.com>
2024-06-11 05:15:09 +0800

    update admin dashboard

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -1,17 +1,16 @@
 @extends('layouts.main')
 @section('container')
 
-<div class="container-fluid p-0">
-
-	<h1 class="h3 mb-3">Ini halaman dashboard admin</h1>
+<h1 class="text-center">Dashboard</h1>
+<hr>
+<h2 class="mb-5">Admin</h2>
 
 	<div class="row">
 		<div class="col-12">
-			<div class="card">
-				<div class="card-body">
-					<div class="row text-center">
-						<div class="col-md-6">
-							<a href="#" class="text-decoration-none">
+					<div class="row">
+						
+						<div class="col-md-6 text-center ">
+							<a href="{{ route("admintempat") }}" class="text-decoration-none">
 								<div class="card">
 									<div class="card-body">
 										<i class="bi bi-house-add"></i>
@@ -20,7 +19,7 @@
 								</div>
 							</a>
 						</div>
-						<div class="col-md-6">
+						<div class="col-md-6 text-center">
 							<a href="{{ route("adminalat") }}" class="text-decoration-none">
 								<div class="card">
 									<div class="card-body">
@@ -30,12 +29,11 @@
 								</div>
 							</a>
 						</div>
-					</div>
-				</div>
-			</div>
+					</div>	
+
 		</div>
 	</div>
 
-</div>
+
 
 @endsection
