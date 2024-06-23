70d4af31518f4f3f0bb9411e3a8090b85c59f171
lelygrande <lelyonmagic@gmail.com>
2024-06-23 13:12:36 +0800

    dashboard merge

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -40,7 +40,7 @@
 				</div>
 			</div>
 		</div>
-		
+
 	</div>
 	<div class="row">
 		<div class="col-6">
@@ -59,7 +59,7 @@
 			<div class="card">
 				<div class="card-body" >
 					<div class="col-md-12 text-center">
-						<a href="#" class="text-decoration-none">							
+						<a href="#" class="text-decoration-none">
 							<i class="bi bi-box-seam"></i>
 							<h5 class="card-title">Peminjaman Alat</h5>
 						</a>
@@ -95,7 +95,7 @@
             <div class="card">
                   <div class="card-body" >
                         <div class="col-md-12 text-center">
-                              <a href="#" class="text-decoration-none">							
+                              <a href="#" class="text-decoration-none">
                                     <i class="bi bi-box-seam"></i>
                                     <h5 class="card-title">Peminjaman Alat</h5>
                               </a>
