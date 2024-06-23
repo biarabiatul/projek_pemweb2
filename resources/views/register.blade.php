6766437a666b5f99ad1dd29e97933ff28620ebae
lelygrande <lelyonmagic@gmail.com>
2024-06-23 15:10:56 +0800

    Penerapan Cookie

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -88,13 +88,6 @@
                                     </div>
                                     @enderror
                                 </div>
-
-                                <div class="d-flex align-items-center">
-                                    <div class="form-check">
-                                        <input type="checkbox" name="remember" id="remember" class="form-check-input">
-                                        <label for="remember" class="form-check-label">Remember Me</label>
-                                    </div>
-                                </div>
                                 <br>
                                 <div class="mb-3">
                                     <div class="d-grid">
